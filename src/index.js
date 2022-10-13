67d2abc8defac22cbc2051399a596eeb4a12038d
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2022-10-13 23:30:42 +0100

    set up webpack configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import image from './assets/image.jpg'
+import './style.css';
+const title = document.createElement('h3');
+title.textContent = 'This is a webpack configuration practice';
+
+const page = document.querySelector('body');
+page.append(title);
+const img = new Image();
+img.src = image;
+page.appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, './src/index.js')
+    }, 
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+              },
+              {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              },   
+        ],
+
+    },
+};
